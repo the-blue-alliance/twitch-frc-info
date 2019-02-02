72e37df920fa9e589ae983f90fe1b88531e7a220
Eugene Fang <fang.eugene@gmail.com>
2019-02-02 11:22:45 -0500

    Disable unused entrypoints

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (_env,argv)=> {
     VideoComponent:{
       path:"./src/VideoComponent.js",
       outputHtml:"video_component.html",
-      build:true
+      build:false
     },
     VideoOverlay:{
       path:"./src/VideoOverlay.js",
