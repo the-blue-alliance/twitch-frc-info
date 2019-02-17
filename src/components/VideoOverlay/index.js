668386efddf0158ec0f16e8cbeb74f2615ddc6a3
Eugene Fang <fang.eugene@gmail.com>
2019-02-17 10:50:35 -0500

    Darken middle panel

--- src/components/VideoOverlay/index.js
+++ src/components/VideoOverlay/index.js
@@ -64,7 +64,7 @@ const MiddlePanel = styled.div`
   width: 65%
   border-radius: 8px;
   color: #fff;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.9);
   box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
 `
 
